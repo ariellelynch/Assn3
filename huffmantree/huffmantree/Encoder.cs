e033c216b37686b74f2ea63df51151c6df642506
arielle <ari.lyn@hotmail.com>
2017-11-22 17:07:14 -0500

    added findfrequencies method and generatehuffmantree method, added dictionary attributes

--- huffmantree/huffmantree/Encoder.cs
+++ huffmantree/huffmantree/Encoder.cs
@@ -1,5 +1,6 @@
 ﻿namespace huffmantree
 {
+    using System.Collections.Generic;
     public class Encoder
     {
         //used for encoding text selections using huffman tree
@@ -8,16 +9,65 @@
         private string EncodedText;
 
         private Node HuffmanTree;
-        
-        //TODO: need two dictionaries, one for finding frequency and another for holding the character codes, maybe another one with key & val reversed for decoding method
+
+        private Dictionary<char, int> frequencies;
+
+        private Dictionary<char, string> codesForChars;
+
+        private Dictionary<string, char> charsForCodes;
         
         //note: P Q contains node/BT elements
         private PriorityQueue<Node> FrequencyQueue;
+
+        private void FindFrequencies()
+        {
+            //finds frequencies of characters and stores them in frequencies dictionary
+            
+            //some sort of try except structure for when key hasn't been created yet/char hasn't been found yet
+            foreach (char c in Plaintext)
+            {
+                //loops over chars and creates new dict entry if none exists already, increments if another instance of char is found
+                try
+                {
+                    //increment frequency value of character
+                    frequencies[c]++;
+                }
+                catch (KeyNotFoundException)
+                {
+                    //create new key for char
+                    frequencies.Add(c, 1);
+                }
+            }
+        }
+
+        private void generateHuffmanTree()
+        {
+            //creates nodes for all characters and adds them into PQ, builds huffman tree
+            foreach (KeyValuePair<char, int> c in frequencies)
+            {
+                //generate node and add to priority 
+                Node newNode = new Node(c.Key, c.Value);
+                FrequencyQueue.Add(newNode);
+            }
+            
+            //now generate huffman tree
+            
+        }
 
         public Encoder()
         {
             //default ctor for encoder
             //takes input from console to initialize plaintext attr.
+            
+            //TODO: initialize plaintext
+
+            EncodedText = "";
+            frequencies = new Dictionary<char, int>();
+            codesForChars = new Dictionary<char, string>();
+            charsForCodes = new Dictionary<string, char>();
+            
+            //don't need to initialize huffman tree because will just contain ref to node initialized later in encode method
+            
         }
         
         //need to write in helper method for traversing tree recursively and creating dictionary of codes
@@ -25,6 +75,8 @@
         public string Encode()
         {
             //uses plaintext attr and returns encoded text
+            FindFrequencies();
+            
         }
 
         public string Decode()
